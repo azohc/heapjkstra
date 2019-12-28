757941a78eb1182fcbfd54116a124d73a80e3761
azohc <juanchozass@gmail.com>
2019-12-28 15:40:21 +0100

    to tty

--- dijkstra-heap/src/Dijkstra.java
+++ dijkstra-heap/src/Dijkstra.java
@@ -4,6 +4,13 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Random;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 /**
  *  
  * @author Juan Chozas Sumbera
@@ -35,7 +42,7 @@ public class Dijkstra {
 			}
 		}
 		
-		while (!Q.vacio()) {
+		while (!Q.vacio() && Q.minValor() != indefinido) {
 			Par<Integer, Integer> pmin = Q.min(); 
 			Q.borrarMin();
 			j = pmin.fst; 				// j = clave del nodo siendo visitado actualmente
@@ -91,7 +98,7 @@ public class Dijkstra {
 	/*
 	 * Genera un grafo de n vertices creando aristas con costes aleatorios
 	 */
-	static Grafo generaGrafo(int n, boolean dirigido, long semilla) {
+	static Grafo generaGrafo(int n, boolean dirigido, long semilla, int umbral) {
 		int[][] matriz = new int[n][n];
 		List<Grafo.Arista> aristas = new ArrayList<>();
 		
@@ -104,9 +111,9 @@ public class Dijkstra {
 		int accn = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < (dirigido ? n : accn); j++) {
-				matriz[i][j] = r.nextInt(150);
-				if (matriz[i][j] > 50) {	// para reducir el numero de aristas	
-					matriz[i][j] = 0;		// coste > 50 == no es arista
+				matriz[i][j] = r.nextInt(100);
+				if (matriz[i][j] < umbral) {	// para reducir el numero de aristas	
+					matriz[i][j] = 0;		
 				}
 			}
 			accn++;
@@ -123,23 +130,87 @@ public class Dijkstra {
 			
 		return new Grafo(dirigido, n, aristas.toArray(new Grafo.Arista[aristas.size()]));
 	}
-	
-	public static void main(String[] args) throws IOException {
-		boolean dirigido = true;
-		int V = 20;
-		long semilla = 11;
 
-		for (int i = 0; i < 4; i++) {
-			
-			Grafo g = generaGrafo(V, dirigido, semilla + i);
-			long t1 = System.nanoTime();
-			
-			Par<int[], int[]> resultados = dijkstra(g, 0);
-			
-			long t2 = System.nanoTime();
-			System.out.println((t2 - t1) / 1000.0f + " ms"); // milisegundos
+	
+	/*
+	 * algoritmo de dijkstra para caminos minimos 
+	 * implementado con monticulo sesgado (con operacion decrecerClave)
+	 * 
+	 * paraemtros: -d 
+	 * grafo dirigido o no
+	 */
+	public static void main(String[] args) throws IOException {
+		Options options = new Options();
+		HelpFormatter formatter = new HelpFormatter();
+		options.addOption("d", false, "generar un grafo dirigido");
+		options.addOption("r", false, "imprimir resultados del algoritmo");
+		options.addOption("i", true, "numero de veces que se ejecuta el algoritmo");
+		options.addOption("n", true, "cantidad de vértices en el grafo");
+		options.addOption("s", true, "semilla para objeto Random");
+		options.addOption("u", true, "porcentage de aristas generadas que se eliminarán");
+		options.addOption("h", false, "imprimir este mensaje");
+
+		
+		String aux;
+		boolean dirigido = false;
+		int V = -1;
+		long semilla = 11;
+		int umbral = 30;
+		int iteraciones = 1;
+		
+		CommandLineParser parser = new DefaultParser();
+		try {
+			CommandLine cmd = parser.parse(options, args);
+			if (cmd.hasOption("h") || cmd.hasOption("help")) {
+				formatter.printHelp("dijkstra -n VS [OPTION...] \n" + 
+						"algoritmo de dijkstra de caminos mínimos\n" + 
+						"genera un grafo de VS vértices forma aleatoria. imprime el tiempo transcurrido en ejecutar el algoritmo de dijkstra\n" + 
+						"\n" + 
+						"Ejemplos:\n" + 
+						"  dijkstra -n 100 -d  			# genera un grafo dirigido de 100 vértices.\n" + 
+						"  dijkstra -n 10 -r			# genera un grafo de 10 vértices e imprime los resultados.\n" + 
+						"  dijkstra -n 10 -s 50			# genera un grafo de 10 vertices. usa 50 como semilla para el objeto Random.\n" +
+						"  dijkstra -n 10 -u 60			# genera un grafo de 10 vertices. el 60% de las aristas generadas se borrarán.\n" +
+						"\n", options);
+				return;
+			}
+			if (!cmd.hasOption("n") || cmd.getOptionValue("n") == null) {
+				System.out.println("error: no se ha especificado el número de vértices. (dijkstra -h para mostrar ayuda)");
+				return;
+			}
+			V = Integer.parseInt(cmd.getOptionValue("n"));
+			dirigido = cmd.hasOption("d"); 
+			if (cmd.hasOption("s")) {
+				aux = cmd.getOptionValue("s");			
+				if (aux != null) {
+					semilla = Long.parseLong(cmd.getOptionValue("s"));			
+				}
+			}
+			if (cmd.hasOption("u")) {
+				aux = cmd.getOptionValue("u");			
+				if (aux != null) {
+					umbral = Integer.parseInt(aux); 
+				}
+			}
+			if (cmd.hasOption("i")) {
+				aux = cmd.getOptionValue("i");			
+				if (aux != null) {
+					iteraciones = Integer.parseInt(aux); 
+				}
+			}
+			Grafo g = generaGrafo(V, dirigido, semilla, umbral);
+			for (int i = 0; i < iteraciones; i++) {				
+				long t1 = System.nanoTime();
+				Par<int[], int[]> resultados = dijkstra(g, 0);
+				long t2 = System.nanoTime();
+				System.out.println((t2 - t1) / 1000.0f); // milisegundos
+				if (cmd.hasOption("r")) {
+					print(resultados);				
+				}
+			}
+		} catch (ParseException e) {
+	        System.err.println("error de parseo: " + e.getMessage());
 		}
-//		print(resultados);	
 	}
 
 }
