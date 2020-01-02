78379394aea449fa1fe0e6177d368b521f51f29c
azohc <juanchozass@gmail.com>
2020-01-02 18:40:56 +0100

    process

--- plots/plots.sh
+++ plots/plots.sh
@@ -6,5 +6,5 @@ set xlabel "nodos"
 set ytics 50000000
 set xtics 250
 set grid
-plot "tiempos" u 1:2 title "dijkstra", "aristas" u 1:(n=$1, a=$2, f(n, a)) with lines title "log"
+plot "tiempos" u 1:2 title "dijkstra", "costes" u 1:2 with lines title "(a+n)log(n)"
 pause -1
