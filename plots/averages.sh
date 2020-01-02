452fcbf0ab6d1ae342a567e5f26795eb0486e7ad
azohc <juanchozass@gmail.com>
2020-01-02 14:54:36 +0100

    scripts

--- plots/averages.sh
+++ plots/averages.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
-# halla las medias de los tiempos de los resultados 
+# halla las medias de los tiempos para cada n distinta
 cd ..
-for file in `ls -v tiempos`;
+for N in `ls -v tiempos`;           # para todas las ejecuciones (N nodos)
 do
 	sum=0
 	first=true
-    second=true
-    for line in $(cat tiempos/$file);
+    second=true                     # de 12 lineas, la primera es el numero de aristas
+    for T in $(cat tiempos/$N);     # hay 11 ejecuciones: 11 tiempos distintos
     do
-    	if $first; then
-    		if $second; then
+    	if $first; then             # no procesar numero de aristas ni
+    		if $second; then        # primera medida (siempre mucho más mayor que las demás)
                 second=false
             else 
                 first=false
             fi
     	else
-			sum=$(awk '{print $1+$2}' <<< "${sum} ${line}")
+			sum=$(awk '{print $1+$2}' <<< "${sum} ${T}") # acumular tiempos
 		fi
     done
-    echo "$file $sum"
+    echo "$N $(awk '{print $1/10}' <<< "${sum}")"        # media entre 10
 done
