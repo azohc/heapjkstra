72a237373e4287e0d350937e9de36978b32909b1
juan chozas <juanchozass@gmail.com>
2020-01-07 20:59:50 +0100

    entrega

--- entrega/fuentes/dijkstra-heap/src/Dijkstra.java
+++ entrega/fuentes/dijkstra-heap/src/Dijkstra.java
@@ -52,11 +52,7 @@ public class Dijkstra {
 			}
 		}
 		
-    // si el vértice mínimo de la cola tiene distancia especial = infinito, significa que 
-    // es un vértice inalcanzable, y que el grafo no es conexo. puede finalizarse el algoritmo porque:
-    //  1: si hubiera mas vértices accesibles, saldrian de la cola de prioridad por tener distancia menor que infinito
-    //  2: los vertices que quedan no pueden mejorar su distancia de infinito por que no son accesibles
-		while (!Q.vacio() && Q.minValor() != indefinido) { 
+		while (!Q.vacio() && Q.minValor() != indefinido) {
 			Par<Integer, Integer> pmin = Q.min(); 
 			Q.borrarMin();
 			j = pmin.fst; 				// j = clave del nodo siendo visitado actualmente
@@ -178,49 +174,50 @@ public class Dijkstra {
 				} else {
 					ejecutarPrueba(Integer.parseInt(cmd.getOptionValue("t")));
 				}
-			}
-			if (!cmd.hasOption("n") || cmd.getOptionValue("n") == null) {
+			} else if (!cmd.hasOption("n") || cmd.getOptionValue("n") == null) {
 				System.out.println("error: no se ha especificado el numero de vértices. (dijkstra -h para mostrar ayuda)");
 				return;
-			}
-			V = Integer.parseInt(cmd.getOptionValue("n"));
-			dirigido = cmd.hasOption("d"); 
-			if (cmd.hasOption("s")) {
-				aux = cmd.getOptionValue("s");			
-				if (aux != null) {
-					semilla = Long.parseLong(cmd.getOptionValue("s"));			
+			} else {
+				V = Integer.parseInt(cmd.getOptionValue("n"));
+				dirigido = cmd.hasOption("d"); 
+				if (cmd.hasOption("s")) {
+					aux = cmd.getOptionValue("s");			
+					if (aux != null) {
+						semilla = Long.parseLong(cmd.getOptionValue("s"));			
+					}
 				}
-			}
-			if (cmd.hasOption("u")) {
-				aux = cmd.getOptionValue("u");			
-				if (aux != null) {
-					umbral = Integer.parseInt(aux); 
+				if (cmd.hasOption("u")) {
+					aux = cmd.getOptionValue("u");			
+					if (aux != null) {
+						umbral = Integer.parseInt(aux); 
+					}
 				}
-			}
-			if (cmd.hasOption("i")) {
-				aux = cmd.getOptionValue("i");			
-				if (aux != null) {
-					iteraciones = Integer.parseInt(aux); 
+				if (cmd.hasOption("i")) {
+					aux = cmd.getOptionValue("i");			
+					if (aux != null) {
+						iteraciones = Integer.parseInt(aux); 
+					}
 				}
-			}
-			Par<Grafo, Integer> grafoGenerado = generaGrafo(V, dirigido, semilla, umbral); 
-			Grafo g = grafoGenerado.fst;
-			Par<int[], int[]> resultados = new Par<int[], int[]>();
-			if (cmd.hasOption("a")) {	// imprimir numero de aristas
-				System.out.println(grafoGenerado.snd);
-			}
-			for (int i = 0; i < iteraciones; i++) {	// solo medir e imprimir tiempo del algoritmo				
-				long t1 = System.nanoTime();
-				resultados = dijkstra(g, 0);
-				long t2 = System.nanoTime();
-				System.out.println((t2 - t1) / 1000.0f); // milisegundos
-			}
-			if (cmd.hasOption("r")) {	// imprimir tabla de resultados
-				print(resultados);				
+				Par<Grafo, Integer> grafoGenerado = generaGrafo(V, dirigido, semilla, umbral); 
+				Grafo g = grafoGenerado.fst;
+				Par<int[], int[]> resultados = new Par<int[], int[]>();
+				if (cmd.hasOption("a")) {	// imprimir numero de aristas
+					System.out.println(grafoGenerado.snd);
+				}
+				for (int i = 0; i < iteraciones; i++) {	// solo medir e imprimir tiempo del algoritmo				
+					long t1 = System.nanoTime();
+					resultados = dijkstra(g, 0);
+					long t2 = System.nanoTime();
+					System.out.println((t2 - t1) / 1000.0f); // milisegundos
+				}
+				if (cmd.hasOption("r")) {	// imprimir tabla de resultados
+					print(resultados);				
+				}
 			}
 		} catch (ParseException e) {
 	        System.err.println("error de parseo: " + e.getMessage());
 		}
+		
 	}
 
 	private static void ejecutarPrueba(int prueba) {
